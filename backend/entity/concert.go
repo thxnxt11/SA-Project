f2d7b437a625f2c4b741c3ec8096b5260d521bd5
thxnxt11 <tanthanat0307@gmail.com>
2025-08-08 17:49:48 +0700

    edit concert.go

--- backend/entity/concert.go
+++ backend/entity/concert.go
@@ -1,11 +1,19 @@
 package entity
 
 import (
-   //"time"
-   "gorm.io/gorm"
+	"time"
+	"gorm.io/gorm"
 )
 
 type Concert struct {
-   gorm.Model
-   Concert 	string 	`json:"concert"`
+	gorm.Model
+	ConcertName string `json:"concert_name"`
+	Artist      string `json:"artist"`
+	OnsaleDate  time.Time `json:"onsale_date"`
+	OffsaleDate time.Time `json:"offsale_date"`
+	VenueID    uint `json:"venue_id"`
+	Venue       *Venue `gorm:"foreignKey:VenueID" json:"venue"`
+	ChartImage  string `json:"chart_image"`
+	UserID uint `json:"user_id"`
+	User *User `gorm:"foreignKey:UserID" json:"user"`
 }
