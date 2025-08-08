6f64869605ffa9966f3ad3e148d9aabfeb4b3fd0
WACHI-19 <wachira1191@gmail.com>
2025-08-08 17:46:26 +0700

    Add files via upload

--- backend/entity/user.go
+++ backend/entity/user.go
@@ -1,24 +1,31 @@
 package entity
 
 import (
-   "time"
-   "gorm.io/gorm"
+	"time"
+
+	"gorm.io/gorm"
 )
 
-type Users struct {
-   gorm.Model
-   FirstName string    `json:"first_name"`
-   LastName  string    `json:"last_name"`
-   Email     string    `json:"email"`
-   Age       uint8     `json:"age"`
-   Password  string    `json:"-"`
-   BirthDay  time.Time `json:"birthday"`
-   Phonenum	 string	   `json:"phonenum"`
-   GenderID  uint      `json:"gender_id"`
-   Gender    *Genders  `gorm:"foreignKey: gender_id" json:"gender"`
-}
+type User struct{
+	gorm.Model
+	FirstName string `json:"staff_first_name"`
+	LastName string	`json:"staff_last_name"`
+	BirthDay  time.Time `json:"birthday"`
+	Age uint8 `json:"staff_age"`
+	Address string `json:"address"`
+	Email string `json:"staff_email"`
+	Password string `json:"-"`
+	Phonenumber string  `json:"satff_phone_number"`
 
-type Genders struct {
-   gorm.Model
-   Gender string `json:"gender"`
+	GenderID uint `json:"gender_id"`
+	Gender *Genders  `gorm:"foreignKey: gender_id" json:"gender"`
+	DepartmentID uint `json:"department_id"`        
+	Department *Department `gorm:"foreignKey: department_id" json:"department"`
+	PositionID uint `json:"position_id"`        
+	Position *Position `gorm:"foreignKey: position_id" json:"position"`
+	RoleID uint `json:"role_id"`        
+	Role *Role `gorm:"foreignKey: role_id" json:"role"`
+
+
+
 }
