8f8b7d6a28dc141afb40b4234537f52bac27f823
thxnxt11 <tanthanat0307@gmail.com>
2025-08-20 15:09:10 +0700

    imprement upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,62 +11,6 @@ import BookingDetail from "./pages/booking/BookingDetail";
 import Payment from "./pages/payment";
 import Concert from "./pages/concert";
 import ConcertDetail from "./pages/concert/concertdetails";
-// import { Card, message } from "antd";
-// import "./App.css";
-
-// import SignUp from "./auth/register";
-// import SignIn from "./auth/login";
-// import { signUp, signIn } from "./api/auth";
-
-// const App: React.FC = () => {
-//   const [isSignup, setIsSignup] = useState(true);
-//   const [msgApi, contextHolder] = message.useMessage();
-
-//   const onFinish = async (values: any) => {
-//     try {
-//       if (isSignup) {
-//         await signUp(values);
-//         msgApi.success("Signup successful!");
-//       } else {
-//         const data = await signIn(values);
-//         msgApi.success(`Token: ${data.token}`);
-//       }
-//     } catch (err: any) {
-//       msgApi.error(err.message || "Something went wrong");
-//     }
-//   };
-
-//   return (
-//     <>
-//       <div className="container">
-//         {contextHolder}
-//         <Card className="card">
-//           <h2 className="title">{isSignup ? "Sign Up" : "Sign In"}</h2>
-
-//           {isSignup ? (
-//             <SignUp onFinish={onFinish} />
-//           ) : (
-//             <SignIn onFinish={onFinish} />
-//           )}
-
-//           <div className="switch">
-//             {isSignup ? "Already have an account? " : "Don't have an account? "}
-//             <a
-//               onClick={() => {
-//                 setIsSignup(!isSignup);
-//                 msgApi.destroy();
-//               }}
-//             >
-//               {isSignup ? "Sign In" : "Sign Up"}
-//             </a>
-//           </div>
-//         </Card>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default App;
 
 const App: React.FC = () => {
   return (
