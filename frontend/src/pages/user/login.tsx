098b16d1aaa83de7e5ef2d1c73513cdfb941e7cd
Yukita1226 <thammarat.songwiwat@gmail.com>
2025-08-23 14:26:18 +0700

    prefinal 2

--- frontend/src/pages/user/login.tsx
+++ frontend/src/pages/user/login.tsx
@@ -1,6 +1,6 @@
 // src/pages/user/login.tsx (only the onFinish + imports)
 import React from "react";
-import { Form, Input, Button, message } from "antd";
+import { Form, Input, Button, message, Card } from "antd";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 
@@ -51,23 +51,50 @@ const SignIn: React.FC = () => {
   };
 
   return (
-    <Form layout="vertical" onFinish={onFinish} requiredMark={false}>
-      <Form.Item label="Email" name="email" rules={[{ required: true, type: "email" }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Password" name="password" rules={[{ required: true }]}>
-        <Input.Password />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit" block size="large">
-          Sign In
-        </Button>
-      </Form.Item>
-      <div style={{ textAlign: "center" }}>
-        No account? <Link to="/signup">Sign up</Link>
-      </div>
-    </Form>
+    <Card
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 500,
+      }}
+    >
+      <h1 style={{fontSize: 30 ,display: "flex",justifyContent: "center"}}>Sign In</h1>
+      <Form layout="vertical" onFinish={onFinish} requiredMark={false}>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            { type: "email", message: "Invalid email" },
+            { pattern: /.+@.+\.com$/, message: "Must end with .com" },
+            { required: true, message: "Please enter your email" },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          hasFeedback
+          rules={[{ required: true, message: "Please enter your password" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" block size="large">
+            Sign In
+          </Button>
+        </Form.Item>
+        <Form.Item>
+          <Link to="/signup">Don't have an account?</Link>
+        </Form.Item>
+      </Form>
+    </Card>
   );
 };
+
 
 export default SignIn;
